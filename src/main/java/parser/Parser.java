a483553d1d031f6881b49409280daf0b56b2deea
gesiel <gesiel.sin@gmail.com>
2015-09-02 22:32:47 -0300

    removing some unnecessary code and adding first real parcer code

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -1,27 +1,42 @@
 package parser;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.Field;
+import java.util.Objects;
 
 /**
  * Created by gesiel on 02/09/15.
  */
 public class Parser {
 
-    protected OutputStream outputStream;
+    protected ByteArrayOutputStream outputStream;
 
     public Parser() {
         outputStream = new ByteArrayOutputStream();
     }
 
-    public Parser(OutputStream outputStream) {
-        if (outputStream != null) this.outputStream = outputStream;
-        else this.outputStream = new ByteArrayOutputStream();
-    }
-
     public OutputStream parse(Object[] pojos) {
         if (pojos == null) throw new IllegalArgumentException("Illegal argument: pojo array cannot be null.");
         else if (pojos.length == 0) throw new IllegalArgumentException("Illegal argument: pojo array cannot be empty.");
+
+        Object first = pojos[0];
+        Class pojoClass = first.getClass();
+        Field[] fields = pojoClass.getFields();
+
+        if (fields.length == 0) return outputStream;
+
+        try {
+            outputStream.write((fields[0].getName() + ";").getBytes());
+            outputStream.write("\n".getBytes());
+            outputStream.write((fields[0].get(first) + ";").getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
 
         return outputStream;
     }
--- src/test/java/parser/ParserTest.java
+++ src/test/java/parser/ParserTest.java
@@ -1,18 +1,17 @@
 package parser;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.MatcherAssert;
+import org.hamcrest.collection.IsEmptyCollection;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.emptyArray;
+import static org.hamcrest.Matchers.hasSize;
 
 /**
  * Created by gesiel on 02/09/15.
@@ -22,28 +21,22 @@ public class ParserTest {
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
 
-    @Test
-    public void testWhenThereIsntOutputStream_shouldCreateANewOne() throws Exception {
-        Parser parser = new Parser();
-        assertThat(parser.outputStream, is(notNullValue()));
+    private Parser parser;
 
-        parser = new Parser(null);
-        assertThat(parser.outputStream, is(notNullValue()));
+    @Before
+    public void setUp() throws Exception {
+        parser = new Parser();
     }
 
     @Test
-    public void testWhenThereIsAnExistingOutputStream_parseShouldReturnIts() throws Exception {
-        OutputStream outputStream = new ByteArrayOutputStream();
-        Parser parser = new Parser(outputStream);
-        assertThat(parser.parse(new Object[]{new Object()}), equalTo(outputStream));
+    public void testWhenThereIsntOutputStream_shouldCreateANewOne() throws Exception {
+        assertThat(parser.outputStream, is(notNullValue()));
     }
 
     @Test
     public void testWhenNullPojoArray_shouldThrowIllegalArgumentException() throws Exception {
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("Illegal argument: pojo array cannot be null.");
-
-        Parser parser = new Parser();
         parser.parse(null);
     }
 
@@ -51,8 +44,30 @@ public class ParserTest {
     public void testWhenEmptyPojoArray_shouldThrowIllegalArgumentException() throws Exception {
         expectedException.expect(IllegalArgumentException.class);
         expectedException.expectMessage("Illegal argument: pojo array cannot be empty.");
-
-        Parser parser = new Parser();
         parser.parse(new Object[] {});
+    }
+
+    @Test
+    public void testWhenThereIsNoAttr_shouldCreateAnEmptyOutputStream() throws Exception {
+        SimplePojo pojo = new SimplePojo();
+        parser.parse(new Object[]{pojo});
+        byte[] bytes = parser.outputStream.toByteArray();
+        assertThat(bytes.length, is(equalTo(0)));
+    }
+
+    @Test
+    public void testWheThereIsJustOneAttr_shouldCreateAnOutputStreamWithOneColumnAndTwoLines() throws Exception {
+        Pojo pojo = new Pojo();
+        parser.parse(new Object[]{pojo});
+        String result = parser.outputStream.toString();
+        assertThat(result, is(equalTo("propertyName;\n0;")));
+    }
+
+    private class SimplePojo {
+
+    }
+
+    private class Pojo {
+        public int propertyName;
     }
 }
