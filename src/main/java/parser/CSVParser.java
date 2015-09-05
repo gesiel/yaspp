776d6fc61aadf4b5f1101975e4a5aaf42d985287
gesiel <gesiel.sin@gmail.com>
2015-09-04 22:59:36 -0300

    logger

--- src/main/java/parser/CSVParser.java
+++ src/main/java/parser/CSVParser.java
@@ -7,35 +7,37 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Created by gesiel on 02/09/15.
  */
 class CSVParser implements Parser {
 
-    public static final String COMMA = ",";
-    public static final String NEW_LINE = "\n";
-    public static final List<Class> PRIMITIVE_CLASSES = Arrays.asList(boolean.class, Boolean.class, byte.class, Byte.class, char.class,
+    private static final String COMMA = ",";
+    private static final String NEW_LINE = "\n";
+    private static final List<Class> PRIMITIVE_CLASSES = Arrays.asList(boolean.class, Boolean.class, byte.class, Byte.class, char.class,
             Character.class, short.class, Short.class, int.class, Integer.class, long.class, Long.class, float.class,
             Float.class, double.class, Double.class, String.class);
 
-    private OutputStreamGateway factory;
+    private static final Logger LOGGER = Logger.getLogger(CSVParser.class.getName());
 
-    public CSVParser(OutputStreamGateway factory) {
-        this.factory = factory;
+    private OutputStreamGateway gateway;
+
+    public CSVParser(OutputStreamGateway gateway) {
+        this.gateway = gateway;
     }
 
     @Override
-    public OutputStream parse(final Object... pojos) {
-        if (pojos == null) throw new IllegalArgumentException("Illegal argument: pojo array cannot be null.");
-        else if (pojos.length == 0) throw new IllegalArgumentException("Illegal argument: pojo array cannot be empty.");
+    public final <T> OutputStream parse(final T... pojos) {
+        validate(pojos);
 
-        Object first = pojos[0];
+        T first = pojos[0];
         Class pojoClass = first.getClass();
         Field[] fields = pojoClass.getFields();
         Method[] methods = pojoClass.getMethods();
 
-        OutputStream outputStream = factory.getOutputStream();
+        OutputStream outputStream = gateway.getOutputStream();
         if (noFieldsToWrite(fields) && noMethodsGetter(methods)) {
             closeOutputStream(outputStream);
             return outputStream;
@@ -48,7 +50,14 @@ class CSVParser implements Parser {
         return outputStream;
     }
 
+    private void validate(Object[] pojos) {
+        LOGGER.info("Validating pojo objects");
+        if (pojos == null) throw new IllegalArgumentException("Illegal argument: pojo array cannot be null.");
+        else if (pojos.length == 0) throw new IllegalArgumentException("Illegal argument: pojo array cannot be empty.");
+    }
+
     private boolean noMethodsGetter(Method[] methods) {
+        LOGGER.info("checking getters methods of primitives");
         for (Method method : methods) {
             if (isGetter(method) && isPrimitive(method.getReturnType())) {
                 return false;
@@ -58,6 +67,7 @@ class CSVParser implements Parser {
     }
 
     private void writePropertiesValuesLines(OutputStream outputStream, Field[] fields, Method[] methods, Object[] pojos) {
+        LOGGER.info("writing fields and methods values");
         for (Object object : pojos) {
             newLine(outputStream);
 
@@ -98,12 +108,14 @@ class CSVParser implements Parser {
     }
 
     private void writeTitleLine(OutputStream outputStream, Field[] fields, Method[] methods) {
+        LOGGER.info("writing fields names");
         for (Field field : fields) {
             if (isPrimitive(field.getType())) {
                 writeColumn(outputStream, field.getName());
             }
         }
 
+        LOGGER.info("writing getters methods names");
         for (Method method : methods) {
             if (isGetter(method) && isPrimitive(method.getReturnType())) {
                 writeColumn(outputStream, cleanGetterName(method));
@@ -129,6 +141,7 @@ class CSVParser implements Parser {
 
     private void closeOutputStream(OutputStream outputStream) {
         try {
+            LOGGER.info("flushing and closing output stream");
             outputStream.flush();
             outputStream.close();
         } catch (IOException e) {
@@ -137,6 +150,7 @@ class CSVParser implements Parser {
     }
 
     private boolean noFieldsToWrite(Field[] fields) {
+        LOGGER.info("checking public and primitives fields to write");
         for (Field field : fields) {
             if (isPrimitive(field.getType())) {
                 return false;
